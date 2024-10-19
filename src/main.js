3ded7fb707121103084bfdc8f19f3d9903bf1d1a
j-pcgoncalves <joao_goncalves_7@hotmail.com>
2024-10-20 00:05:20 +0100

    create main menu scene and Sonic entity

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import k from "./kaplayCtx";
 import disclaimer from "./scenes/disclaimer";
+import mainMenu from "./scenes/mainMenu";
+
+k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
+k.loadSprite("platforms", "graphics/platforms.png");
+k.loadSprite("sonic", "graphics/sonic.png", {
+	sliceX: 8,
+	sliceY: 2,
+	anims: {
+		run: { from: 0, to: 7, loop: true, speed: 30 },
+		jump: { from: 8, to: 15, loop: true, speed: 100 },
+	},
+});
 
 k.loadFont("mania", "fonts/mania.ttf");
 
+k.loadSound("jump", "sounds/Jump.wav");
+
 k.scene("disclaimer", disclaimer);
+k.scene("main-menu", mainMenu);
 
 k.go("disclaimer");
